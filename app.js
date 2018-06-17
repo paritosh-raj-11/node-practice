9a7a1d9fa42c6db320827a90da02a8bd3fd6cf90
Paritosh <paritosh.raj@daffodilsw.com>
2018-06-18 01:22:20 +0530

    updates

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const hbs = require('hbs');
+app.set("view engine",'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.send('Hello Express!!')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        details:"detail will go be here"
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        statusCode:"404",
+        error : "bad request"
+    })
+})
+
+app.listen(PORT);
+console.log("express app running ...");
+
+
